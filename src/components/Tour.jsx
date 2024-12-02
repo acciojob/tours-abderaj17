4f4b799769365fa1ca0f388191f3161ed0054952
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-12-02 20:46:53 +0530

    com

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -11,11 +11,11 @@ const Tour = ({id, name , info, image, price, removeTour}) => {
         <h4 className='tour-price'>${price}</h4>
         <p>
             {showMore ? info : `${info.substring(0, 200)}...`}
-            <button onClick={()=> setShowMore(!showMore)} className='show-more-button'>
+            <button id={`tour-item-para-${id}`} onClick={()=> setShowMore(!showMore)} className='show-more-button'>
                 {showMore ? 'Show less' : 'show more'}
             </button>
         </p>
-        <button onClick={()=> removeTour(id)} className='remove-tour-button'>
+        <button onClick={()=> removeTour(id)} id={`delete-btn-${id}`} className='remove-tour-button'>
             Not Interested
         </button>
     </div>
